d14bde5d8bd99b682439064442e381881165f6bd
Handi Rachmawan <mail.handira@gmail.com>
2025-08-15 10:53:32 +0700

    refactor: remove old App module and controller, introduce Server module and controller

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ServerModule } from './server/module/server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
+    ServerModule,
     new FastifyAdapter(),
   );
   await app.listen(process.env.PORT ?? 3000);
