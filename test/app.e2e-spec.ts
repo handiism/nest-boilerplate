62ae77ed8295105cd5ab690ed4e4716793aaadc0
Handi Rachmawan <mail.handira@gmail.com>
2025-08-15 13:42:22 +0700

    refactor: update e2e tests to use ServerModule instead of AppModule

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { App } from 'supertest/types';
-import { AppModule } from './../src/app.module';
+import { ServerModule } from './../src/server/module/server.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [ServerModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
